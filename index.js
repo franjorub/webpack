1c230783b87f4a751f29fad9b105ca21f485841e
Jhon <jneira@hyosystem.com>
2019-01-23 14:56:17 -0500

    reorganizacion

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
-console.log('100')
-console.log('100')
-console.log('100')
-console.log('100')
-console.log('100')
-console.log('100')
-
-console.log('asdasdasd')
-console.log('last')
 
 async function test1() {
     await console.log('test1')
+    console.log('100')
+    console.log('100')
+    console.log('100')
+    console.log('100')
+    console.log('100')
+    console.log('100')
+    
+    console.log('asdasdasd')
+    console.log('last')
+    
+    /////////////////////
+    console.log('100')
+    console.log('100')
 }
 
-/////////////////////
-console.log('100')
-console.log('100')
+function xxx () {
+    return 'return '
+}
