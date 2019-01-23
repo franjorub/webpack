3d9bc15f2825735a6a5f19bd2f67a46ee02c013e
Jhon <jneira@hyosystem.com>
2019-01-23 15:46:14 -0500

    asdadasdsad

--- index.js
+++ index.js
@@ -31,3 +31,71 @@ function xxx () {
 
 console.log('requerimiento terminado3331222')
 console.log('requerimiento terminado 221')
+
+
+async function test1() {
+    await console.log('test1')
+    console.log('modify')
+    console.log('modify')
+    console.log('modify')
+    console.log('modify')
+    console.log('modify')
+    console.log('modify')
+    
+    console.log('asdasdasd')
+    console.log('last')
+    
+//////////////////////////
+    console.log('100')
+    console.log('100')
+}
+
+function xxx () {
+    return 'return '
+}
+function xxx () {
+    return 'return '
+}
+function xxx () {
+    return 'return '
+}
+function xxx () {
+    return 'return '
+}
+
+console.log('requerimiento terminado3331222')
+console.log('requerimiento terminado 221')
+
+
+async function test1() {
+    await console.log('test1')
+    console.log('modify')
+    console.log('modify')
+    console.log('modify')
+    console.log('modify')
+    console.log('modify')
+    console.log('modify')
+    
+    console.log('asdasdasd')
+    console.log('last')
+    
+//////////////////////////
+    console.log('100')
+    console.log('100')
+}
+
+function xxx () {
+    return 'return '
+}
+function xxx () {
+    return 'return '
+}
+function xxx () {
+    return 'return '
+}
+function xxx () {
+    return 'return '
+}
+
+console.log('requerimiento terminado3331222')
+console.log('requerimiento terminado 221')
