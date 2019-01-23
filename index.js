d61d92b751a5c5bfc64abb80fec7e07022806c9d
Jhon <jneira@hyosystem.com>
2019-01-23 14:50:40 -0500

    delete1.5

--- index.js
+++ index.js
@@ -11,19 +11,3 @@ console.log('last')
 async function test1() {
     await console.log('test1')
 }
-
-function test2() {
-    console.log('test2')
-}
-
-function test3() {
-    console.log('test3')
-}
-
-async function x1 () {
-    await test1()
-    await test2()
-    await test2()
-}
-
-x1()
