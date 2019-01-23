f2f388b40b7fdd2b57a8a926d9c62f26d695343c
Jhon <jneira@hyosystem.com>
2019-01-23 15:39:42 -0500

    add 1

--- index.js
+++ index.js
@@ -29,5 +29,5 @@ function xxx () {
     return 'return '
 }
 
-console.log('requerimiento terminado333')
-console.log('requerimiento terminado 22')
+console.log('requerimiento terminado3331')
+console.log('requerimiento terminado 221')
